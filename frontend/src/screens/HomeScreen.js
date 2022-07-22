1e1eee92c3eb2ff77f7a360426e7b55e7b9ec7a6
Shernille Licud <shernillelicud@Shernilles-MBP.Home>
2022-07-22 18:08:26 +0200

    Add Rating Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,7 +10,7 @@ const HomeScreen = () => {
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
+            <Product key={product._id} product={product} />
           </Col>
         ))}
       </Row>
