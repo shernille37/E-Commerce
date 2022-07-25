d553e472fb15444be0873919996e859fd86f418c
Shernille Licud <shernillelicud@Shernilles-MBP.Home>
2022-07-26 01:02:39 +0200

    Setup MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
 
 dotenv.config();
+
+//Connect to Database
+connectDB();
 
 //Initialize Express
 const app = express();
