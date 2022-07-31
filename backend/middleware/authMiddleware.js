6189b093c1a490dd40a58cfeda887e9b2b970d15
Shernille Licud <shernillelicud@Shernilles-MBP.Home>
2022-07-31 22:40:03 +0200

    Implement jsonwebtoken with the authMiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import asyncHandler from 'express-async-handler';
+
+export const protect = asyncHandler(async (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findById(decoded._id).select('-password');
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorized, token failed!');
+    }
+  } else {
+    res.status(401);
+    throw new Error('Not authorized, no token!');
+  }
+});
