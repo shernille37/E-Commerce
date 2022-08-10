ab5cf9a57304da8cc89c208ece7247adb35d7c04
Shernille Licud <shernillelicud@Shernilles-MBP.Home>
2022-08-10 16:31:04 +0200

    Add BreadCrumbs to Check Out Process

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { saveShippingAddress } from '../reducers/cartReducers';
+import LoginScreen from '../screens/LoginScreen';
 
 const ShippingScreen = () => {
+  const authUser = useSelector((state) => state.user.authUser);
   const shippingAddress = useSelector((state) => state.cart.shippingAddress);
 
   const dispatch = useDispatch();
@@ -39,58 +42,69 @@ const ShippingScreen = () => {
   };
 
   return (
-    <FormContainer>
-      <h1>Shipping</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='address'>
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter Address'
-            value={address}
-            required
-            onChange={(e) => setAddress(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+    <>
+      {!authUser ? (
+        <>
+          <CheckoutSteps step1 /> <LoginScreen />
+        </>
+      ) : (
+        <>
+          <FormContainer>
+            <CheckoutSteps step2 />
+            <h1>Shipping</h1>
+            <Form onSubmit={submitHandler}>
+              <Form.Group controlId='address'>
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter Address'
+                  value={address}
+                  required
+                  onChange={(e) => setAddress(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-        <Form.Group controlId='city'>
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter City'
-            value={city}
-            required
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+              <Form.Group controlId='city'>
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter City'
+                  value={city}
+                  required
+                  onChange={(e) => setCity(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-        <Form.Group controlId='postalCode'>
-          <Form.Label>Postal Code</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter Postal Code'
-            value={postalCode}
-            required
-            onChange={(e) => setPostalCode(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+              <Form.Group controlId='postalCode'>
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter Postal Code'
+                  value={postalCode}
+                  required
+                  onChange={(e) => setPostalCode(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-        <Form.Group controlId='country'>
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter Country'
-            value={country}
-            required
-            onChange={(e) => setCountry(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+              <Form.Group controlId='country'>
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter Country'
+                  value={country}
+                  required
+                  onChange={(e) => setCountry(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-        <Button className='mt-3' type='submit' variant='primary'>
-          Continue
-        </Button>
-      </Form>
-    </FormContainer>
+              <Button className='mt-3' type='submit' variant='primary'>
+                Continue
+              </Button>
+            </Form>
+          </FormContainer>
+        </>
+      )}
+    </>
   );
 };
 
