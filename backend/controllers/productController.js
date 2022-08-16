f74e136e941a8f2f0cea38a49cb53a1e2fa34c56
Shernille Licud <shernillelicud@Shernilles-MBP.Home>
2022-08-16 16:31:48 +0200

    Implement Delete Product Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,19 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc Delete product by ID
+// @route DELETE /api/product/:id
+// @access PRIVATE/Admin
+const deleteProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product deleted successfully' });
+  } else {
+    res.status(404);
+    throw new Error('Product not Found');
+  }
+});
+
+export { getProducts, getProductById, deleteProductById };
