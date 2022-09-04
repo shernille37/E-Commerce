7978e0535d03775ff6099aad133ca7fa0b46928b
Shernille Licud <shernillelicud@Shernilles-MBP.Home>
2022-09-04 23:54:36 +0200

    Prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is Running');
-});
-
 // Product Routes
 app.use('/api/products', productRoutes);
 
@@ -49,6 +45,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is Running');
+  });
+}
 
 //--Middlewares--
 
